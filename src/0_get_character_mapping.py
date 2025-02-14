b51e0eae0b19d50e540f907983ff1a5c827169a1
kaiusdepaula <kaiusdepaulacorrea@gmail.com>
2025-02-14 00:40:58 -0300

    feat: reworks on character mapping stage

--- src/0_get_character_mapping.py
+++ src/0_get_character_mapping.py
@@ -1,93 +1,56 @@
-import requests
-import os
+# import requests
 from PIL import Image
-from io import BytesIO
-from bs4 import BeautifulSoup
+import numpy as np
+from transformers import AutoModel
+from random import random, sample
+import torch
+import os
 
-# Google API KEY (Make sure to create one and save it in .env)
-from dotenv import load_dotenv
-load_dotenv()
-GOOGLE_CSE_ID = os.getenv('GOOGLE_CSE_ID')
-GOOGLE_API_KEY= os.getenv('GOOGLE_API_KEY')
-
-from saga_mapper_helper import OnePieceSaga
+from utils import OnePieceSaga, read_image
 one_piece = OnePieceSaga()
+
+model = AutoModel.from_pretrained("ragavsachdeva/magiv2", trust_remote_code=True).cuda().eval()
+
+# Helper function to crop from pages random characters
+def save_bbox(image: np.ndarray, bbox: list[float], saga: str) -> None:
+    pil_image = Image.fromarray(image)
+    x1, y1, x2, y2 = map(int, bbox)
+    crop = Image.fromarray(image[y1:y2, x1:x2])
+    filename = f"character_images/{saga}/{hash(random())}.png"
+    try:
+        crop.save(filename)
+    except Exception as e:
+        print(f"Failed with error: {e}")
 
 # Output folder
 OUTPUT_DIR = "character_images"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
+def sample_and_save_cropped_characters(sampled_pages:int) -> None:
+    """Sample pages and volumes from whole sagas and save the identified rectangle on a folder for manual labelling evaluation."""
+    if not sampled_pages:
+        sampled_pages = 60
 
-# This is not useful as most characters don't speak, and some have a design change.
-def get_one_piece_characters() -> list:
-    """Parse the webpage from OnePiece fandom to return a list of all characters."""
-
-    url = "https://onepiece.fandom.com/wiki/List_of_Canon_Characters" 
-    response = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(response.text, "html.parser")
-    table = soup.tbody
-
-    characters = []
-    for row in table.find_all("tr")[1:]: # Remove header
-        first_link = row.find("a") # get all anchor elements
-        
-        if first_link:
-            character_name = first_link.get("title") #return only first cell
-            characters.append(character_name)
-
-    return set(characters)
-
-def search_character_image(character:str, saga:str) -> str:
-    """Use Google Custom Search API to find an image URL for a character."""
-    search_url = "https://www.googleapis.com/customsearch/v1"
-    params = {
-        "q": character + "One Piece Character image" + saga.replace("_", " "),
-        "cx": GOOGLE_CSE_ID,
-        "key": GOOGLE_API_KEY,
-        "searchType": "image",
-        "num": 1,
-    }
-    response = requests.get(search_url, params=params)
-    if response.status_code == 200:
-        results = response.json()
-        if "items" in results:
-            return results["items"][0]["link"]  # First image result
-    return None
-
-
-def download_and_save_image(character, image_url, saga) -> None:
-    """From a given adress, attempt to download an image to a given character and save it."""
-    try:
-        response = requests.get(image_url, stream=True)
-        if response.status_code == 200:
-            image = Image.open(BytesIO(response.content))# Remove special case
-            filename = f"{OUTPUT_DIR}/{saga}/{character.replace("/", "-")}.png"
-            image.save(filename, format="PNG")
-            print(f"✅ Saved: {filename}")
-        else:
-            print(f"❌ Failed to download: {image_url}")
-    except Exception as e:
-        print(f"❌ Error processing {character}: {e}")
-
-def fetch_and_save_images():
-    sagas = one_piece.character_mapper
-    for saga, characters in sagas.items():
+    for saga, volumes in one_piece.saga_mapper.items():
         os.makedirs(f"{OUTPUT_DIR}/{saga}", exist_ok=True)
-        for character in characters:
-            # Check if image already exists.
-            if f"{character}.png" in os.listdir(f"{OUTPUT_DIR}/{saga}"):
-                print(f"📑 {character} found in cache folder, skipping.")
-                continue
-            image_url = search_character_image(character, saga)
-            if image_url:
-                print(f"📸 Found image for {character}: {image_url}")
-                download_and_save_image(character, image_url, saga)
-            else:
-                print(f"⚠️ No image found for {character}")
+        volume_lists = []  
+        for volume in volumes:  
+            volume_lists.extend([f"data/v{volume:03d}/{page}" for page in os.listdir(f"data/v{volume:03d}")])  
+        sample_pages = sample(volume_lists, sampled_pages)
+
+        chapter_pages = [read_image(x) for x in sample_pages]
+        character_bank={"images":[], "names":[]}
+        with torch.no_grad():
+            per_page_results = model.do_chapter_wide_prediction(chapter_pages, character_bank, use_tqdm=True, do_ocr=True)
+
+        for image, page_result in zip(chapter_pages, per_page_results):
+            model.visualise_single_image_prediction(image, page_result)
+            for bbox in page_result["characters"]:
+                save_bbox(image, bbox, saga)
+
+
+
 # Run script
 if __name__ == "__main__":
-    # fetch_and_save_images()
-    print(search_character_image("Nami", "East Blue"))
+    # Use code below with caution!
+    sample_and_save_cropped_characters(sampled_pages=50) 
--- src/1_text_extraction.py
+++ src/1_text_extraction.py
@@ -1,11 +1,8 @@
-from PIL import Image
-import numpy as np
 from transformers import AutoModel
 import torch
 import os
-import re
 
-from saga_mapper_helper import OnePieceSaga
+from utils import OnePieceSaga, read_image
 one_piece = OnePieceSaga()
 
 os.makedirs("outputs", exist_ok=True)
@@ -13,48 +10,40 @@ os.makedirs("transcripts", exist_ok=True)
 
 model = AutoModel.from_pretrained("ragavsachdeva/magiv2", trust_remote_code=True).cuda().eval()
 
-def read_image(path_to_image):
-    with open(path_to_image, "rb") as file:
-        image = Image.open(file).convert("L").convert("RGB")
-        image = np.array(image)
-    return image
+# volume = "002"
+volumes = [f"{i:03d}" for i in range(2, 3)]
+for volume in volumes:
+    os.makedirs(f"outputs/v{volume}", exist_ok=True)
+    os.makedirs(f"transcripts/v{volume}", exist_ok=True)
+    chapter_pages = [f"data/v{volume}/{path}" for path in os.listdir(f"data/v{volume}")]
 
-def extract_num(path):
-    match = re.search(r'(\d+)(?=\D*$)', path)
-    return int(match.group(1)) if match else float('inf')
+    saga = one_piece.get_saga_by_volume(int(volume))
+    characters = one_piece.get_characters_by_volume(int(volume))
 
-volume = "001"
-os.makedirs(f"outputs/v{volume}", exist_ok=True)
-os.makedirs(f"transcripts/v{volume}", exist_ok=True)
-chapter_pages = sorted([f"data/v{volume}/{path}" for path in os.listdir(f"data/v{volume}")], key=extract_num)
-
-saga = one_piece.get_saga_by_volume(int(volume))
-characters = one_piece.get_characters_by_volume(int(volume))
-
-character_bank = {
-    "images": [f"character_images/{saga}/{character}.png" for character in characters],
-    "names": characters
-}
-
-chapter_pages = [read_image(x) for x in chapter_pages]
-character_bank["images"] = [read_image(x) for x in list(character_bank["images"])]
-
-with torch.no_grad():
-    per_page_results = model.do_chapter_wide_prediction(chapter_pages, character_bank, use_tqdm=True, do_ocr=True)
-
-transcript = []
-for i, (image, page_result) in enumerate(zip(chapter_pages, per_page_results)):
-    model.visualise_single_image_prediction(image, page_result, f"outputs/v{volume}/page_{i}.png")
-    speaker_name = {
-        text_idx: page_result["character_names"][char_idx] for text_idx, char_idx in page_result["text_character_associations"]
+    character_bank = {
+        "images": [f"character_images/{saga}/{character}.png" for character in characters],
+        "names": characters
     }
-    for j in range(len(page_result["ocr"])):
-        if not page_result["is_essential_text"][j]:
-            continue
-        name = speaker_name.get(j, "unsure") 
-        transcript.append(f"<{name}>: {page_result['ocr'][j]}")
+
+    chapter_pages = [read_image(x) for x in chapter_pages]
+    character_bank["images"] = [read_image(x) for x in list(character_bank["images"])]
+
+    with torch.no_grad():
+        per_page_results = model.do_chapter_wide_prediction(chapter_pages, character_bank, use_tqdm=True, do_ocr=True)
+
+    transcript = []
+    for i, (image, page_result) in enumerate(zip(chapter_pages, per_page_results)):
+        model.visualise_single_image_prediction(image, page_result, f"outputs/v{volume}/page_{i}.png")
+        speaker_name = {
+            text_idx: page_result["character_names"][char_idx] for text_idx, char_idx in page_result["text_character_associations"]
+        }
+        for j in range(len(page_result["ocr"])):
+            if not page_result["is_essential_text"][j]:
+                continue
+            name = speaker_name.get(j, "unsure") 
+            transcript.append(f"<{name}>: {page_result['ocr'][j]}")
 
 
-with open(f"transcripts/v{volume}/transcript.txt", "w") as fh:
-    for line in transcript:
-        fh.write(line + "\n")
+    with open(f"transcripts/v{volume}/transcript.txt", "w") as fh:
+        for line in transcript:
+            fh.write(line + "\n")
